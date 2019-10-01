a72a7550ce69735c5a4d7591b849f43d941fd991
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2019-10-01 15:15:12 -0300

    1173

--- URI/main.c
+++ URI/main.c
@@ -390,7 +390,7 @@ int main()
     *   1172 - Iniciante (Substituição em Vetor I) -- VETORES
     */
 
-    int X[10];
+    /*int X[10];
     int i = 0;
     for(i = 0; i < 10; i++){
         scanf("%d", &X[i]);
@@ -402,6 +402,21 @@ int main()
             X[i] = 1;
         }
         printf("X[%d] = %d\n", i, X[i]);
+    }
+    return 0;*/
+
+    /**
+    *   1173 - Iniciante (Preenchimento de Vetor I) -- VETORES
+    */
+
+    int V, N[10];
+    scanf("%d", &V);
+    N[0] = V;               ///Atribuindo a primeira posição
+    printf("N[0] = %d\n", N[0]);
+    int i = 0;
+    for(i = 1; i < 10; i++){
+        N[i] = N[i - 1] * 2;
+        printf("N[%d] = %d\n", i, N[i]);
     }
     return 0;
 }
