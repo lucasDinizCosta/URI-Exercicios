c378087ce8e749c167b5bd482ecf2142ee03f6f6
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2020-01-13 10:16:40 -0300

    1037

--- URI/main.c
+++ URI/main.c
@@ -453,4 +453,27 @@ int main()
         printf("R2 = %.5lf\n", ((-B - pow(delta, 0.5))/(2*A)));
     }
     return 0;*/
+
+    /**
+    *   1037 - Iniciante (Intervalo) --- CONDICIONAIS
+    */
+
+    double number;
+    scanf("%lf", &number);
+    if((number >= 0) && (number <= 25)){
+        printf("Intervalo [0,25]\n");
+    }
+    else if((number > 25) && (number <= 50)){
+        printf("Intervalo (25,50]\n");
+    }
+    else if((number > 50) && (number <= 75)){
+        printf("Intervalo (50,75]\n");
+    }
+    else if((number > 75) && (number <= 100)){
+        printf("Intervalo (75,100]\n");
+    }
+    else{
+        printf("Fora de intervalo\n");
+    }
+    return 0;
 }
